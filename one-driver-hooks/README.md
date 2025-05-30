bc9dfafb50a27ea8c29c7776092b40ab62108749
Balazs Nemeth <bnemeth@opennebula.io>
2025-05-30 13:54:27 +0200

    add hardcoded VNET driver hooks
    These hooks facilitate to add the assigned public IP
    to the NIC and add a host route on the KVM host.
    NOTE: all values are hardcoded, so the deployment only
    works for a specific host.
    
    Signed-off-by: Balazs Nemeth <bnemeth@opennebula.io>

---
+++ one-driver-hooks/README.md
@@ -0,0 +1,5 @@
+# OpenNebula driver hooks
+
+This folder contains the IONOS-specific driver hooks that are installed to IONOS deployment.
+
+More information of the driver developments is [here](https://docs.opennebula.io/stable/integration_and_development/infrastructure_drivers_development/index.html)
---
+++ one-driver-hooks/vnm/bridge/pre.d/01-add-route-public-ip.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export PATH=$HOME/ionosctl:$PATH
+
+# --force is required to override the existing config file, if it exists
+ionosctl login --force --token 'TOKEN_PLACEHOLDER'
+
+ionosctl nic update --datacenter-id  7ff493e4-c722-4429-80f3-0c90564c26c3\
+ --server-id d84f8a6b-bee0-4b24-82d5-4c28ea97e84d\
+ --nic-id 07f0a82e-0f41-47d2-834d-729748399608\
+ --ips '217.154.225.209,217.154.225.193'
+
+# Note: NOPASSWORD rule is needed for the oneadmin to add/delete routes
+sudo ip route add 217.154.225.193/32 dev br0
+
+ionosctl logout
